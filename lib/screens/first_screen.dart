b17862fd588f6949b59f207fce23b5ccd39073ff
Rameen Khan <rameenkhan710244@gmail.com>
2024-03-11 14:41:52 -0700

    first screen created

---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
